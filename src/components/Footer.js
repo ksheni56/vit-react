e2e8f79e7ab04247e803ea66d87fe4a25250e7a8
Svitx <simon@vicetoken.com>
2018-09-26 12:48:05 -0400

    Corrected on mobile, removed extra classes and converted to Router Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
     render() {
         return(
-            <footer className="page-footer fixed-bottom font-small blue">
-                <div className="container-fluid text-center text-md-left">
-                    <div className="row">
-                        <div className="col-md-12 mt-md-0 mt-3 text-right">
-                            <a href="/privacy">Privacy Policy</a> | <a href="/2257">2257 Statement</a>
-                        </div>
-                    </div>
-                </div>
+            <footer className="page-footer fixed-bottom text-center">
+                <Link style={{'color': 'white'}} to="/privacy">Privacy Policy</Link>
+                &nbsp;|&nbsp;
+                <Link style={{'color': 'white'}} to="/2257">2257 Statement</Link>
             </footer>
         );
     }
